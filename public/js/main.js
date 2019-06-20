9be5bec51d0886ec5bfc6c05e330499f2d557145
Affan <h.xain@yahoo.com>
2019-06-20 11:56:48 +0500

    Remove Debug Alert

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 function confirmDelete(id) {
     var answer = prompt("Type 'DELETE' to delete!");
-    alert(answer.toLowerCase());
     if ('delete' === answer.toLowerCase()){
         document.querySelector('#del'+id).submit();
     }
