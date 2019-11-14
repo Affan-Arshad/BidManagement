0a0944aaec3424c178fbe1574b7872d825dbc322
Abdulla Azman <abdullaazman1991@gmail.com>
2019-11-14 10:32:59 +0500

    - Dashboard Changed to Vue - Hidden

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang=en>
+
+<head>
+    <meta charset=utf-8>
+    <meta http-equiv=X-UA-Compatible content="IE=edge">
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <link rel=icon href=/client/favicon.ico> <link rel=stylesheet type=text/css
+        href=https://use.fontawesome.com/releases/v5.7.0/css/all.css> <title>client</title>
+    <link href=/client/css/chunk-vendors.b89365ac.css rel=preload as=style>
+    <link href=/client/js/app.8b62c636.js rel=preload as=script>
+    <link href=/client/js/chunk-vendors.e7020b5b.js rel=preload as=script>
+    <link href=/client/css/chunk-vendors.b89365ac.css rel=stylesheet>
+</head>
+
+<body><noscript><strong>We're sorry but client doesn't work properly without JavaScript enabled. Please enable it to
+            continue.</strong></noscript>
+    <div id=app></div>
+    <script src=/client/js/chunk-vendors.e7020b5b.js> </script> <script src=/client/js/app.8b62c636.js> </script>
+        </body> </html>
